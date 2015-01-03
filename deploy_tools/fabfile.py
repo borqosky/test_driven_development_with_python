48a3ab5153db1d1cb5982a9c490b4eab1e0e2fbb
Wlodzimierz Borkowski <wlodek.borkowski@gmail.com>
2015-01-03 22:37:17 +0000

    fab file works ok now

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -24,8 +24,8 @@ def _update_settings(source_folder, site_name):
     settings_path = source_folder + '/superlists/settings.py'
     sed(settings_path, "DEBUG = True", "DEBUG = False")
     sed(settings_path,
-        "ALLOWED_HOSTS =.+$",
-        "ALLOWED_HOSTS = ['%s']" % (site_name,))
+        'ALLOWED_HOSTS =.+$',
+        'ALLOWED_HOSTS = ["%s"]' % (site_name,))
     secret_key_file = source_folder + '/superlists/secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
@@ -49,7 +49,7 @@ def _update_static_files(source_folder):
 
 
 def _update_database(source_folder):
-    run('cd $s && ../virtualenv/bin/python3 manage.py migrate' % (
+    run('cd %s && ../virtualenv/bin/python3 manage.py migrate' % (
         source_folder,))
 
 
