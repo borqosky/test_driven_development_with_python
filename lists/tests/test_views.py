1f8045deb55b4639462ff6380d703fbdafb62cbc
Wlodzimierz Borkowski <wlodek.borkowski@gmail.com>
2015-01-08 16:33:47 +0000

    validation model and form at once

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,3 +1,5 @@
+from unittest import skip
+
 from django.test import TestCase
 from django.utils.html import escape
 
@@ -16,6 +18,13 @@ class HomePageTest(TestCase):
 
 
 class ListViewTest(TestCase):
+    def post_invalid_input(self):
+        list_ = List.objects.create()
+        return self.client.post(
+            '/lists/%d/' % (list_.id,),
+            data={'text': ''}
+        )
+
     def test_uses_list_template(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/%d/' % (list_.id,))
@@ -89,12 +98,19 @@ class ListViewTest(TestCase):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    def post_invalid_input(self):
-        list_ = List.objects.create()
-        return self.client.post(
-            '/lists/%d/' % (list_.id,),
-            data={'text': ''}
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            '/lists/%s/' % (list1.id,),
+            data={'text': 'textey'}
         )
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
 
 class NewListTest(TestCase):
