62332d25bb0ff58ff901fcecc5b52a737baeb95f
Wlodzimierz Borkowski <wlodek.borkowski@gmail.com>
2015-01-28 17:59:01 +0000

    logging in settings and auth func

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,4 +1,5 @@
 import requests
+import logging
 
 from django.contrib.auth import get_user_model
 
@@ -10,8 +11,12 @@ DOMAIN = 'localhost'
 
 class PersonaAuthenticationBackend(object):
     def authenticate(self, assertion):
+        logging.warning('entering authenticate function')
         response = requests.post(PERSONA_VERIFY_URL,
                                  data={'assertion': assertion, 'audience': DOMAIN})
+        logging.warning('got response from persona')
+        logging.warning(response.content.decode())
+
         if response.ok and response.json()['status'] == 'okay':
             email = response.json()['email']
             try:
