28243c737edc9250f7bbf820b55711b94d17aed0
Wlodzimierz Borkowski <wlodek.borkowski@gmail.com>
2015-02-03 11:11:18 +0000

    property on a name in list, and test

--- lists/models.py
+++ lists/models.py
@@ -6,8 +6,15 @@ from django.conf import settings
 class List(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
 
+    def name(self):
+        return self.item_set
+
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
+
+    @property
+    def name(self):
+        return self.item_set.first().text
 
 
 class Item(models.Model):
